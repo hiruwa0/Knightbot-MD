7cd813f7e4a57e5c6898f14003e61be0ccb0a496
hiruwa0 <122200542+hiruwa0@users.noreply.github.com>
2025-03-10 09:45:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Hiruwa',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Hiruwa Bot",
+  botOwner: 'HiRUWA-MD', // Your name
+  ownerNumber: '94704349801', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
